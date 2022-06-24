9ec2ed9d35b3ee7ef1fd73563ed870af4817665c
musaddique333 <mohammedmusaddiquemogal@gmail.com>
2022-06-24 18:09:12 +0530

    final chnage

--- healthy_programmmer.py
+++ healthy_programmmer.py
@@ -4,18 +4,12 @@ import numpy as np
 
 
 def task_done(music, file_name, message):
-    temp = True
     music.play(-1)
-    print("Enter stop to stop the music and enter the log")
-    answer = input()
-    while temp:
-        if answer == "stop":
-            temp = False
-            music.stop()
     print(message)
     with open(f"{file_name}", "a") as f:
         f.write(f"reminded at {time.ctime()}\n")
     done_work = input()
+    music.stop()
     with open(f"{file_name}", "a") as f:
         f.write(f"\t{done_work} at {time.ctime()}\n\n")
 
@@ -26,20 +20,8 @@ if __name__ == '__main__':
     eyes_music = pygame.mixer.Sound("audio/eyes.mp3")
     exercise_music = pygame.mixer.Sound("audio/exercise.mp3")
 
-    exercise_array = np.array([9.45, 10.30, 11.15, 12.0, 12.45, 1.30, 2.15, 3.0, 3.45, 4.30])
-
-    eyes = time.ctime().split()
-    water = time.ctime().split()
-    exercise = time.ctime().split()
-
-    eyes = eyes[3].split(":")
-    water = water[3].split(":")
-    exercise = exercise[3].split(":")
-
-    eyes = int(eyes[1])
-    water = int(water[1])
-    exercise.pop()
-    exercise = float(f"{exercise[0]}.{exercise[1]}")
+    exercise_array1 = np.array([9.45, 10.3, 11.15, 12.0, 12.45, 13.3, 14.15, 15.0, 15.45, 16.3, 17.15, 18.0, 18.45, 19.3, 20.15, 21.0, 21.45, 22.3, 23.15, 0.0, 0.45, 1.3, 2.15, 3.0, 3.45, 4.3, 5.15, 6.0, 6.45, 7.3, 8.15, 9.0])
+    exercise_array2 = np.array([12.0, 15.0, 18.0, 21.0, 0.0, 3.0, 6.0, 9.0])
     c1, c2, c3, c4, c5 = 0, 0, 0, 0, 0
     while True:
         eyes = time.ctime().split()
@@ -55,7 +37,7 @@ if __name__ == '__main__':
         exercise.pop()
         exercise = float(f"{exercise[0]}.{exercise[1]}")
 
-        if water == 0 and eyes == 0 and exercise == 12.0 or water == 0 and eyes == 0 and exercise == 3.0:
+        if water == 0 and eyes == 0 and exercise in exercise_array2:
             if c1 == 0:
                 c1 = 1
                 c2, c3, c4, c5 = 0, 0, 0, 0
@@ -82,7 +64,7 @@ if __name__ == '__main__':
                 c1, c2, c3, c5 = 0, 0, 0, 0
                 task_done(eyes_music, "eyes.txt", "Say Eyes done if done with eyes exercise")
 
-        elif exercise in exercise_array:
+        elif exercise in exercise_array1:
             if c5 == 0:
                 c5 = 1
                 c1, c2, c3, c4 = 0, 0, 0, 0
